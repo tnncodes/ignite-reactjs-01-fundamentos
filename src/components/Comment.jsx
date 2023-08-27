113236e74f9b7ffc9b3d0062f6d6e077a54fe74e
tnncodes <tnncodes@gmail.com>
2023-08-27 17:34:27 -0300

    Realizando likes nos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { ThumbsUp, Trash } from '@phosphor-icons/react';
-import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -28,9 +36,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
